24c7b6bb956d0e5ac7dccf4e5f973d04519c1389
Артём Куркин <artykn88@gmail.com>
2022-10-14 11:54:52 +0300

    commit23

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql_psycopg2'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
