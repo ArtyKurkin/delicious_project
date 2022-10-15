fbddb087c9d097fbdebb4525282f2e429f3d7d1a
Артём Куркин <artykn88@gmail.com>
2022-10-15 12:55:50 +0300

    commit34

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default=5432)
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
