526651cda2fd902eed7f8b68ee2655031375c30f
Артём Куркин <artykn88@gmail.com>
2022-10-17 17:53:03 +0300

    commit37

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,41 +4,40 @@ from django.db import models
 
 
 class User(AbstractUser):
-
-    username_validator = RegexValidator(
-        regex=r'^[\w.@+-]+\Z',
-        message=(
-            "Username должен содержать только буквы, "
-            "цифры и символы: '@', '.', '+', '-', '_'"
-        )
-    )
-    email = models.EmailField(
-        'Электронная почта',
-        max_length=254,
-        unique=True
-    )
-    username = models.CharField(
-        "Логин",
-        max_length=150,
-        unique=True,
-        validators=[username_validator, ]
-    )
-    first_name = models.CharField(
-        'Имя',
-        max_length=150,
-    )
-    last_name = models.CharField(
-        'Фамилия',
-        max_length=150,
-    )
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
+    email = models.EmailField(
+        'Адрес электронной почты',
+        max_length=254,
+        unique=True
+    )
+
+    username = models.CharField(
+        'Уникальный никнейм',
+        max_length=150,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+\Z',
+            ),
+        ]
+    )
+
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+
     class Meta:
-        ordering = ['-id']
+        ordering = ('id',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
-        return self.email
+        return self.username
