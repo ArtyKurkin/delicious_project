2adccd9697d4f16529632782396179058623d9a3
Артём Куркин <artykn88@gmail.com>
2022-10-21 22:46:09 +0300

    commit63

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
+#from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
