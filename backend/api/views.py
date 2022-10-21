3b2203b71308f339c81be972c41692f7a97a1624
Артём Куркин <artykn88@gmail.com>
2022-10-21 23:06:11 +0300

    commit66

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
@@ -112,7 +112,7 @@ class RecipesViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrAdminOrReadOnly, )
     filter_backends = (DjangoFilterBackend, )
     filterset_class = RecipeFilter
-    pagination_class = LimitOffsetPagination
+    pagination_class = PageNumberPagination
 
     @staticmethod
     def _add_recipe(model, request, pk):
