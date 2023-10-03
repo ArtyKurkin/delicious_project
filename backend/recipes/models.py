938d8fabcb2ace8aebf20294557e4c7151f3f28a
Artem Kurkin <artykn88@gmail.com>
2023-10-03 19:01:46 +0300

    Update models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -152,6 +152,9 @@ class IngredientRecipe(models.Model):
 
 
 class ShoppingCart(models.Model):
+    """
+    Shopping cart model
+    """
     user = models.ForeignKey(
         User,
         related_name='shopping_cart',
